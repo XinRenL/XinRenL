811436618dffefe5c0d75f1b892c0171ed426bb2
Richard ren <2871390589@qq.com>
2023-07-22 12:08:27 +0800

    Add files via upload
    Concentration Calculation of BBI Gold Nanoparticles Using a UV Spectrophotometer

---
+++ AuUV10nm.m
@@ -0,0 +1,10 @@
+% 输入摩尔吸光系数，光路程（cm），和吸光度
+molar_extinction_coefficient = 95500000; % L*mol-1*cm-1
+path_length = 1; % cm
+absorbance = input("请输入吸光度值：");
+
+% 计算浓度
+concentration = absorbance / (molar_extinction_coefficient * path_length) * 1e9;
+
+% 输出结果
+disp(['物质浓度为 ' num2str(concentration) ' nmol/L']);
